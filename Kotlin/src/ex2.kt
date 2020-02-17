07426a7f39bef859a5863ae6b6130637fb7f6a50
kirillmega000 <43070302+kirillmega000@users.noreply.github.com>
2020-02-17 12:56:32 +0300

    17.02.20

---
+++ Kotlin/src/ex2.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main(args:Array<String>) {
+    val first_name:String? = readLine()
+    val last_name:String? = readLine()
+
+    println("Hello, ${first_name} ${last_name}!")
+    val sc:Scanner
+}
---
+++ Kotlin/src/ex3.kt
@@ -0,0 +1,6 @@
+fun sum(a:Int = 0,b:Int=7):Int{
+    return a+b
+}
+fun main(args: Array<String>) {
+    println(sum(a=9))
+}
