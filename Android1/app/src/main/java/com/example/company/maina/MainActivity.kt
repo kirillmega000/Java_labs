9be2c5890ef999bb3d20645ccf88cde5e82b59d7
kirillmega000 <kirill.petuhov.00@mail.ru>
2020-03-19 13:33:00 +0300

    last

--- Android1/app/src/main/java/com/example/company/maina/MainActivity.kt
+++ Android1/app/src/main/java/com/example/company/maina/MainActivity.kt
@@ -72,11 +72,12 @@ class MainActivity : AppCompatActivity() {
                 != PackageManager.PERMISSION_GRANTED ||ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_NETWORK_STATE)
                 != PackageManager.PERMISSION_GRANTED ||ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_WIFI_STATE)
                 != PackageManager.PERMISSION_GRANTED ||ContextCompat.checkSelfPermission(this,Manifest.permission.INTERNET)
-                != PackageManager.PERMISSION_GRANTED) {
+                != PackageManager.PERMISSION_GRANTED ||ContextCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH)
+                != PackageManager.PERMISSION_GRANTED)  {
             println("Requesting permission")
             ActivityCompat.requestPermissions(this,
                     arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO,Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_NETWORK_STATE,Manifest.permission.ACCESS_WIFI_STATE,Manifest.permission.INTERNET  ), 0)
+                            Manifest.permission.ACCESS_NETWORK_STATE,Manifest.permission.ACCESS_WIFI_STATE,Manifest.permission.INTERNET ,Manifest.permission.BLUETOOTH ), 0)
         }
     }
 
