09246f594380e73d9f5a1098e351a90794f7b202
kirillmega000 <kirill.petuhov.00@mail.ru>
2020-07-23 13:32:50 +0300

    PrimalWorkingWithComments
    Элементарные операции с комментами

--- CalculatorSource/src/Calculator.java
+++ CalculatorSource/src/Calculator.java
@@ -6,144 +6,72 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) throws ScriptException {
         Scanner sc=new Scanner(System.in);
-          /*  ScriptEngineManager manager = new ScriptEngineManager();
-            ScriptEngine engine = manager.getEngineByName("JavaScript");
-        System.out.println(engine.eval(expression));*/
             String expression = sc.nextLine();
             System.out.println(primalSequence(expression));
-
-
-       /* sequence=sc.nextLine();
-        System.out.println(countMaxPriority(sequence));*/
     }
-    public static int countMaxPriority(String seq){//поиск максимального приоритета операции(в скобочках)
-        int n = 0;
-        int maxn = 0;
-        for(int i=0;i<seq.length();i++){
-            if(seq.charAt(i)=='('){
-                n += 1;
-                if(n>maxn) maxn=n;
-            }
-            else if(seq.charAt(i)==')'){
-                n -= 1;
-            }
-        }
-        return maxn;
-    }
-    public static String primalSequence(String seq){//парсинг элементарного выражения(без скобок)
+     private static String primalSequence(String seq){//парсинг элементарного выражения(без скобок)
         int i=0;
-            while(i<seq.length()-1) {
+        while(i<seq.length()-1) {//парсинг умножения и деления(по правилам математики они первые)
                 i+=1;
                 if (seq.charAt(i) == '*') {
-                    System.out.println(seq.charAt(i));
-                    int n1 = i - 1;//поиск границ для элементарного умножения
-                    int n2 = i + 1;
-                    while (((seq.charAt(n1) > 47) && (seq.charAt(n1) < 58) && (n1 > 0)) || (seq.charAt(n1) == 46)) {
-                        n1 -= 1;
-
-                    }
-                    while (((seq.charAt(n2) > 47) && (seq.charAt(n2) < 58) && (n2 < seq.length() - 1)) || (seq.charAt(n2) == 46)) {
-                        n2 += 1;
-                    }
-                    System.out.println(i + "  " + n1 + "  " + n2 + "  ");
-                    if (n1 != 0)
-                        n1 += 1;
-                    if (n2 != seq.length() - 1)
-                        n2 -= 1;
-                    System.out.println(Float.parseFloat(seq.substring(n1, i)));
-                    System.out.println(Float.parseFloat(seq.substring(i + 1, n2 + 1)));
-                    Float num = Float.parseFloat(seq.substring(n1, i)) * Float.parseFloat(seq.substring(i + 1, n2 + 1));
-                    System.out.println(num);
-                    String seqprev = seq.substring(0, n1);
-                    String seqnext = seq.substring(n2 + 1, seq.length());
-                    seq = seqprev + num.toString() + seqnext;
-                    i=n1;
+                    Pair fin=binaryOperation(seq,'*',i);
+                    seq=fin.seq;
+                    i=fin.num;
                 }
 
                 if (seq.charAt(i) == '/') {
-                    System.out.println(seq.charAt(i));
-                    int n1 = i - 1;//поиск границ для элементарного умножения
-                    int n2 = i + 1;
-                    while (((seq.charAt(n1) > 47) && (seq.charAt(n1) < 58) && (n1 > 0)) || (seq.charAt(n1) == 46)) {
-                        n1 -= 1;
-
-                    }
-                    while (((seq.charAt(n2) > 47) && (seq.charAt(n2) < 58) && (n2 < seq.length() - 1)) || (seq.charAt(n2) == 46)) {
-                        n2 += 1;
-                    }
-                    System.out.println(i + "  " + n1 + "  " + n2 + "  ");
-                    if (n1 != 0)
-                        n1 += 1;
-                    if (n2 != seq.length() - 1)
-                        n2 -= 1;
-                    System.out.println(Float.parseFloat(seq.substring(n1, i)));
-                    System.out.println(Float.parseFloat(seq.substring(i + 1, n2 + 1)));
-                    Float num = Float.parseFloat(seq.substring(n1, i)) / Float.parseFloat(seq.substring(i + 1, n2 + 1));
-                    System.out.println(num);
-                    String seqprev = seq.substring(0, n1);
-                    String seqnext = seq.substring(n2 + 1, seq.length());
-                    seq = seqprev + num.toString() + seqnext;
-                    i=n1;
+                    Pair fin=binaryOperation(seq,'/',i);
+                    seq=fin.seq;
+                    i=fin.num;
                 }
             }
             i=0;
-            System.out.println(seq);
-       while(i<seq.length()-1){
+       while(i<seq.length()-1){//парсинг сложения и вычитания
               i+=1;
             if(seq.charAt(i)=='+'){
-                System.out.println(seq.charAt(i));
-                int n1 = i - 1;//поиск границ для элементарного умножения
-                int n2 = i + 1;
-                while(((seq.charAt(n1)>47)&&(seq.charAt(n1)<58)&&(n1>0))||(seq.charAt(n1)==46)) {
-                    n1 -= 1;
-
-                }
-                while(((seq.charAt(n2)>47)&&(seq.charAt(n2)<58)&&(n2<seq.length()-1))||(seq.charAt(n2)==46)) {
-                    n2 += 1;
-                }
-                System.out.println(i+"  "+n1+"  "+n2+"  ");
-                if(n1!=0)
-                n1+=1;
-                if(n2!=seq.length()-1)
-                    n2-=1;
-                System.out.println(Float.parseFloat(seq.substring(n1,i)));
-                System.out.println(Float.parseFloat(seq.substring(i+1,n2+1)));
-                Float num=Float.parseFloat(seq.substring(n1,i))+Float.parseFloat(seq.substring(i+1,n2+1));
-                System.out.println(num);
-                String seqprev=seq.substring(0,n1);
-                String seqnext=seq.substring(n2+1,seq.length());
-                seq=seqprev+num.toString()+seqnext;
-                i=n1;
-                System.out.println(seq);
+                Pair fin=binaryOperation(seq,'+',i);
+                seq=fin.seq;
+                i=fin.num;
             }
             if(seq.charAt(i)=='-'){
-                System.out.println(seq.charAt(i));
-                int n1 = i - 1;//поиск границ для элементарного умножения
-                int n2 = i + 1;
-                while(((seq.charAt(n1)>47)&&(seq.charAt(n1)<58)&&(n1>0))||(seq.charAt(n1)==46)) {
-                    n1 -= 1;
-
-                }
-                while(((seq.charAt(n2)>47)&&(seq.charAt(n2)<58)&&(n2<seq.length()-1))||(seq.charAt(n2)==46)) {
-                    n2 += 1;
-                }
-                System.out.println(i+"  "+n1+"  "+n2+"  ");
-                if(n1!=0)
-                n1+=1;
-                if(n2!=seq.length()-1)
-                    n2-=1;
-                System.out.println(Float.parseFloat(seq.substring(n1,i)));
-                System.out.println(Float.parseFloat(seq.substring(i+1,n2+1)));
-                Float num=Float.parseFloat(seq.substring(n1,i))-Float.parseFloat(seq.substring(i+1,n2+1));
-                System.out.println(num);
-                String seqprev=seq.substring(0,n1);
-                String seqnext=seq.substring(n2+1,seq.length());
-                seq=seqprev+num.toString()+seqnext;
-                i=n1;
-                System.out.println(seq);
+                Pair fin=binaryOperation(seq,'-',i);
+                seq=fin.seq;
+                i=fin.num;
             }
         }
-        System.out.println();
+
         return seq;
     }
+     private static Pair binaryOperation(String seq,char op,int i){
+        int n1 = i - 1;//поиск границ для элементарной операции
+        int n2 = i + 1;
+        while(((seq.charAt(n1)>47)&&(seq.charAt(n1)<58)&&(n1>0))||(seq.charAt(n1)==46)) {
+            n1 -= 1;
+
+        }
+        while(((seq.charAt(n2)>47)&&(seq.charAt(n2)<58)&&(n2<seq.length()-1))||(seq.charAt(n2)==46)) {
+            n2 += 1;
+        }
+         //сдвиг границ операции(если мы не с краю строки)
+        if(n1!=0)
+            n1+=1;
+        if(n2!=seq.length()-1)
+            n2-=1;
+        //сама операция
+        Float num= Float.valueOf(0);
+        if(op=='*')
+            num=Float.parseFloat(seq.substring(n1,i))*Float.parseFloat(seq.substring(i+1,n2+1));
+        if(op=='/')
+            num=Float.parseFloat(seq.substring(n1,i))/Float.parseFloat(seq.substring(i+1,n2+1));
+        if(op=='+')
+            num=Float.parseFloat(seq.substring(n1,i))+Float.parseFloat(seq.substring(i+1,n2+1));
+        if(op=='-')
+            num=Float.parseFloat(seq.substring(n1,i))-Float.parseFloat(seq.substring(i+1,n2+1));
+        //возвращение значений(сама преобразованная строка и номер символа для продолжения преобразования строки);
+        String seqprev=seq.substring(0,n1);
+        String seqnext=seq.substring(n2+1,seq.length());
+        seq=seqprev+num.toString()+seqnext;
+        return new Pair(n1,seq);
+    }
 }
+
---
+++ CalculatorSource/src/Pair.java
@@ -0,0 +1,8 @@
+public class Pair {
+    int num;
+    String seq;
+    Pair(int num,String seq){
+        this.num=num;
+        this.seq=seq;
+    }
+}
